38b302881f77ab45667daa2b9dc5fdb0aa130029
Neo <DavidEddlemon@Gmail.com>
2013-01-07 16:47:31 -0600

    Update vigor.mk

--- vigor.mk
+++ vigor.mk
@@ -140,3 +140,5 @@ $(call inherit-product-if-exists, vendor/htc/vigor/vigor-vendor.mk)
 $(call inherit-product, device/htc/msm8660-common/msm8660.mk)
 $(call inherit-product, frameworks/base/build/phone-hdpi-512-dalvik-heap.mk)
 $(call inherit-product, build/target/product/full_base_telephony.mk)
+PRODUCT_NAME := htc_vigor
+PRODUCT_DEVICE := vigor
